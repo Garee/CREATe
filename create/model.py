5ba72fabe4ef01d3aa839df7da51c24971ab9f5c
Gordon Martin <gordon.hugh.martin@gmail.com>
2016-05-14 11:05:06 +0100

    Proper json file ;x

--- create/model.py
+++ create/model.py
@@ -1,29 +1,36 @@
 import requests
 import json
+import time
 
 model = []
 
-def query(request):
+# A generator that returns all the pages from the wiki
+def allPagesGenerator(request):
     request['action'] = 'query'
     request['format'] = 'json'
     lastContinue = {'continue': ''}
-    while True:
+
+    # Time it out because i have no idea how to use the API properly
+    timeStart = time.time()
+    while time.time() < timeStart + 120 :
         # Clone original request
         req = request.copy()
         # Modify it with the values returned in the 'continue' section of the last result.
         req.update(lastContinue)
         # Call API
-        result = requests.get('http://www.copyrightevidence.org/evidence-wiki/api.php', params=req).json()
-        result = json.dumps(result)
-        yield result
+        resultPy = requests.get('http://www.copyrightevidence.org/evidence-wiki/api.php', params=req).json()
+
+        yield resultPy
 
 # Converts a page model into our internal model
 def makePageModel(pageJson):
     print("stuff")
 
 if __name__ == '__main__':
-    print("[")
-    for result in query({'generator': 'allpages', 'formatversion': '2', 'prop': 'revisions', 'rvprop':'content'}):
-        print(result)
-        print(",")
-    print("]")
+    results = []
+    for result in allPagesGenerator({'generator': 'allpages', 'formatversion': '2', 'prop': 'revisions', 'rvprop':'content'}):
+        results.append(result)
+
+    asJson = json.dumps(results)
+
+    print(asJson)
